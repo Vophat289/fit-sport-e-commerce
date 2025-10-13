c741e301d8d49cc879feff56d73e1600fc6c077f
Vo Hong Phat <vohongphat2892005@gmail.com>
2025-10-13 22:19:44 +0700

    chuyen code

---
+++ frontend/src/app/thanhloi/thanhloi.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-thanhloi',
+  imports: [],
+  templateUrl: './thanhloi.component.html',
+  styleUrl: './thanhloi.component.css'
+})
+export class ThanhloiComponent {
+
+}
