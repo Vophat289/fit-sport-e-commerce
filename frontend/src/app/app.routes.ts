b1dfe270ffdd1e5c6ac2939f3c2e956fe8ac9460
Vo Hong Phat <vohongphat2892005@gmail.com>
2025-10-13 11:28:22 +0700

    them header va footer

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component:HomeComponent  },
+    { path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: '**', redirectTo: '/home'}
+];
