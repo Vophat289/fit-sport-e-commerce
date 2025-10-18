5966c5f713567e740e617f2e52f8993362deccd8
Vo Hong Phat <vohongphat2892005@gmail.com>
2025-10-18 21:55:13 +0700

    update local front method and credentials

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,11 @@ import productRoutes from './routes/product.routes.js';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin:"http://localhost:4200", //connect với frontend
+    method: ["GET", "POST", "DELETE"],
+    credentials: true // gửi cookie, token, jwt để xác thực
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
--- backend/src/server.js
+++ backend/src/server.js
@@ -9,6 +9,7 @@ connectDB();
 
 const PORT = process.env.PORT || 3000;
 
+
 app.listen(PORT, () => {
     console.log(`Server đang chạy tại PORT: ${PORT}`);
 })
