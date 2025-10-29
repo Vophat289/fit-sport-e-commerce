448e91fdd0ad031642027241c02a128d24ab9100
Vo Hong Phat <vohongphat2892005@gmail.com>
2025-10-29 22:06:48 +0700

    update app.config.ts categoryService  feature

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,10 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-     provideRouter(routes, withComponentInputBinding())]//withcomponentinputbiding cho phép truyền các component qua route
+    provideRouter(routes, withComponentInputBinding()),//withcomponentinputbiding cho phép truyền các component qua route
+    provideHttpClient()]
 };
