c74ede4ae964035d31ac6458f98e8aeb025d4d50
Vo Hong Phat <vohongphat2892005@gmail.com>
2025-11-12 08:52:07 +0700

    update

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
 
+
 import { HomeCategoryComponent } from './components/home-category/home-category.component';
+import { ProductPageComponent } from './pages/product-page/product-page.component'
 
 export const routes: Routes = [
     { path: 'home', component:HomeComponent  },
-    { path: '', redirectTo: '/home', pathMatch: 'full'},
-    { path: 'category/:slug', component: HomeCategoryComponent}, //category client
-    { path: '**', redirectTo: '/home'}
+    { path: '', redirectTo: 'home', pathMatch: 'full'},
+    { path: 'category/:slug', component: HomeCategoryComponent},
+    { path: 'products', component: ProductPageComponent}, 
+    { path: 'products/category/:slug', component: ProductPageComponent},
+    { path: '**', redirectTo: 'home'}
 ];
--- frontend/src/app/pages/product-page/product-page.component.ts
+++ frontend/src/app/pages/product-page/product-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {ProductService, Product} from '@app/services/product.service';
+import {  CategoryService , Category} from '@app/services/category.service'
 
 @Component({
   selector: 'app-product-page',
@@ -12,11 +13,12 @@ export class ProductPageComponent implements OnInit{
 
   products: Product[] = [];
   loading: boolean = true;
-  categories: any[] = [];
-  selectdCategory: string | null = null;
+  categories: Category[] = [];
+  selectedCategory: string | null = null; //lưu lại danh mục khi chọn sp
 
-  constructor(private productService: ProductService) {}
+  constructor(private productService: ProductService, private categoryService: CategoryService) {}
 
+  //lifecycle hook
   ngOnInit(): void { 
     this.loadProducts();
     this.loadCategories();
@@ -24,10 +26,10 @@ export class ProductPageComponent implements OnInit{
   
   loadProducts(): void {
     this.loading = true;
-    this.productService.getAll().subscribe({
+    this.productService.getAll().subscribe({ //subscribe nó nhận dữ liệu khi api hoàn thành 
       next: (data) => {
         this.products = data;
-        this.categories = false;
+        this.loading = false;
         console.log('Sản phẩm đã tải: ', data);
       },
       error: (err) => {
@@ -37,12 +39,39 @@ export class ProductPageComponent implements OnInit{
     });
   }
 
+
   //lấy danh mục
   loadCategories(): void {
-    this.productService.getProductsByCategory().subscribe({
-      next: (data) => (this.categories = data),
+    this.categoryService.getAll().subscribe({
+      next: (data) => { this.categories = data},
       error: (err) => console.error('Lỗi tải danh mục', err)
-      
     });
+  }
+
+  //lọc sp theo danh mục
+  filterByCategory(slug: string): void {
+    this.loading = true;
+    this.selectedCategory = slug;
+    this.productService.getByCategory(slug).subscribe({
+      next: (data) => { 
+        this.products = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error('Không lọc được sản phẩm ', err);
+        this.loading = false;
+      }
+    });
+  }
+
+  //xem lại all sp
+  resetFilter(): void {
+    this.selectedCategory = null;
+    this.loadProducts();
+  }
+
+  //test add cart
+   addToCart(product: Product): void {
+    console.log('🛒 Đã thêm vào giỏ:', product.name);
   }
 }
