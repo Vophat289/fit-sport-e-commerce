0c8aed41a8e7ca2c3e39e16fca325949375ca562
Vo Hong Phat <vohongphat2892005@gmail.com>
2025-11-13 19:48:34 +0700

    update trang san pham , getProductsByCategory ở controller feature

--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import {getAllProducts,getProductBySlug, createProduct, updateProduct, deleteProduct} from "../controllers/product.controller.js";
+import {getAllProducts,getProductBySlug,getProductsByCategory, createProduct, updateProduct, deleteProduct} from "../controllers/product.controller.js";
 import upload from '../middlewares/upload.middleware.js';
 const router = express.Router();
 
 
 router.get("/", getAllProducts);
-router.get("/:slug", getProductBySlug)
+router.get("/:slug", getProductBySlug);
+router.get("category/:slug", getProductsByCategory)
+
 router.post("/",upload.array("images", 5), createProduct);
 router.post("/:id",upload.array("images", 5), updateProduct);
 router.delete("/:id", deleteProduct);
