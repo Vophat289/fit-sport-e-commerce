7fa0e9a76c923b24c898931c5a0ce3771eff625a
Tuan <thanhtuan30032005@gmail.com>
2025-12-13 00:05:44 +0700

    hoàn chỉnh reviewreview

--- backend/src/routes/voucher.routes.js
+++ backend/src/routes/voucher.routes.js
@@ -3,16 +3,8 @@ import * as voucherController from "../controllers/voucher.controller.js";
 
 const router = express.Router();
 
-<<<<<<< HEAD
 router.get("/", voucherController.getAvailable);
 router.post("/validate", voucherController.validate); 
 router.post("/use", voucherController.useVoucher);
 router.post("/apply", voucherController.applyVoucher);
 export default router;
-=======
-router.get("/", voucherController.getAvailable);               
-router.post("/validate", voucherController.validate);
-router.post("/use", voucherController.useVoucher);
-
-export default router;
->>>>>>> origin/feature/review_product
